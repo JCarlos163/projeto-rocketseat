b70b8c5232059cc88b1ead9cf9a96e35ef20d20b
Carlos <jeanrewstei@gmail.com>
2024-06-15 20:48:28 -0300

    update infos

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   // pegar a tag img
   const image = document.querySelector("#profile img")
-  
+
   // substituir a imagem
-  if(html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    image.setAttribute('src', './assets/avatar-light.png')
+    image.setAttribute("src", "./assets/avatar-light.png")
     image.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos de lente escura e jaqueta preta, sem barba e fundo degrade de azul e rosa."
+      "Foto de Carlos Souza com bone branco e camisa preta, olhando para o celular, com fundo azul e corrente prata."
     )
   } else {
     // se tiver sem light mode, manter a imagem normal
-    image.setAttribute('src', './assets/avatar.png')
+    image.setAttribute("src", "./assets/avatar.png")
     image.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos com lente transparente e jaqueta preta, sem barba e fundo degrade de azul e rosa."
+      "Foto de Carlos Souza com bone e camisa vermelha, olhando para baixo, com fundo azul e corrente prata."
     )
   }
-
-
 }
